19da20901b0fa680bb7d449ba8d6f709df94ff92
Yash C <yashchauhan132017@gmail.com>
2025-02-19 13:43:45 -0500

    updates

--- vip_job3.sh
+++ vip_job3.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=diffusion_job          # Job name
-#SBATCH --output=diffusion_output1.log     # Output log file
-#SBATCH --error=diffusion_error.log       # Error log file
+#SBATCH --job-name=optimization_job          # Job name
+#SBATCH --output=optimization_output.log     # Output log file
+#SBATCH --error=optimization_error.log       # Error log file
 #SBATCH --partition=ice-cpu                   # Partition (change based on your cluster)
 #SBATCH --gres=gpu:1                      # Request 1 GPU
-#SBATCH --mem-per-cpu=128G               # Increase memory (try 32G, 64G, or higher)
+#SBATCH --mem-per-gpu=64G               # Increase memory (try 32G, 64G, or higher)
 #SBATCH --cpus-per-task=8                 # Request 4 CPU cores
 #SBATCH --time=01:00:00                   # Time limit (1 hour)
 #SBATCH --mail-type=END,FAIL              # Email on job end or fail
@@ -15,4 +15,4 @@ module load anaconda3
 source activate my_env  # Replace with your Conda environment
 
 # Run the Python script
-accelerate launch bf_test.py --num_processes=1
+python 
